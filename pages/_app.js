112d8b9ddf1ca8dda8dde99f0ba51e1dd3c66633
isabelalk <isabelalorentekr@gmail.com>
2021-07-13 12:55:55 -0300

    Meu primeiro commit

---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  /* Reset CSS */
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+`
+const theme = {
+  colors: {
+    primary: 'red',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
